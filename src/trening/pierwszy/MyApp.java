d41cf6f9216d2bf2a6a4464f98b4bdbe226e6c51
Damian <Damian@DESKTOP-0SAAVPI>
2018-02-28 19:51:29 +0100

    + XML

--- src/trening/pierwszy/MyApp.java
+++ src/trening/pierwszy/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp
 
 	public static void main(String[] args) 
 	{
-		Coach theCoach = new BaseBallCoach();
+		Coach theCoach = new TrackCoach();
 		
 		System.out.println(theCoach.getDailyWorkout());
 
